37ade93dc9c36cc741db2ea67c2a0c44ecc13ab1
IchwanArdi <ichwanpwt22@gmail.com>
2025-08-04 09:12:39 +0700

    fixed bug

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,9 +1,12 @@
-// components/NotFoundPage.jsx
+import { useSettings } from '../contexts/SettingsContext';
+
 export default function NotFoundPage() {
+  const { darkMode } = useSettings();
+
   return (
-    <div className="not-found text-center flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">404 - Halaman Tidak Ditemukan</h1>
-      <p className="mt-2 text-gray-600">Maaf, halaman yang kamu cari tidak tersedia.</p>
+    <div className={`not-found text-center flex flex-col items-center justify-center h-screen ${darkMode ? 'bg-black/20' : 'bg-white/20'}`}>
+      <h1 className={`text-4xl font-bold ${darkMode ? 'text-white' : 'text-black'}`}>404 - Halaman Tidak Ditemukan</h1>
+      <p className={`mt-2 ${darkMode ? 'text-gray-400' : 'text-gray-600'}`}>Maaf, halaman yang kamu cari tidak tersedia.</p>
     </div>
   );
 }
